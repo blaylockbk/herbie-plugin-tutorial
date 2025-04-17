1cd77f6ae72c560ff4028dcb440bc33c9e3404b7
Brian Blaylock <blaylockbk@gmail.com>
2025-04-16 22:31:12 -0700

    created two custom model templates

--- README.md
+++ README.md
@@ -31,7 +31,75 @@ I created this `herbie-data-tutorial` repository using [uv](https://docs.astral.
 uv init --lib herbie-plugin-tutorial --python 3.10
 ```
 
-Then I created a repo of the same name on GitHub and updated the remote.
+Then I added herbie-data as a dependency (the plugin would be useless if it didn't have herbie-data installed).
 
+```bash
+uv add herbie-data
+```
 
+Finally, I created the repo on GitHub and gave it an MIT license so I can share it with you.
 
+## Update pyproject.toml
+
+Next, add entry points to `pyproject.toml` so Herbie knows this package is a plugin. The key and value in the second line should match your plugin name.
+
+```toml
+[project.entry-points."herbie.plugins"]
+herbie_plugin_demo = "herbie_plugin_demo"
+```
+
+## Create your custom model template
+
+Next, create your custom model templates. These are placed in the package's `__init__.py` file.
+
+Look at that file for the two examples:
+
+1. `hrrr_analysis` - Custom behavior for accessing HRRR model (only discover files on AWS and only analysis fields)
+2. `bmw` - Local GRIB2 files output from a fictional model _BMW_ "Brian's Model of Weather"
+
+These model templates are picky in their format. Here are some things to remember:
+
+1. The class is the name a Herbie uses calls the model (i.e., `model='bmw'`). **The class name _must_ be lower case!** Herbie changes the user input to lower case, so if they call `model='HRRR'` it will actually get `model='hrrr'`.
+
+1. `self.DESCRIPTION` and `self.DETAILS` are just convenient metadata.
+
+1. `self.PRODUCTS` must have at least one entry. If a Herbie user doesn't specify a `product=None`, then the first is used by default. This value is available to build the file path as `self.product`.
+
+1. `self.SOURCES` can have one or more key-value pairs. Herbie will loop through each SOURCE until it finds a file that exists.
+
+1. Look at [existing model templates](https://github.com/blaylockbk/Herbie/tree/main/herbie/models) for more complex examples.
+
+## Install your plugin
+
+From the root of your new plugin (the directory the pyproject.toml file exists) install the package with pip
+
+```bash
+pip install -e .
+```
+
+## Use your custom model in Herbie
+
+Once installed, you can then access your custom model templates in Herbie.
+
+```python
+from herbie import Herbie
+```
+
+Prints the following to confirm you have custom templates available:
+
+```
+Herbie: model template 'hrrr_analysis' from custom plugin was added to globals.
+Herbie: model template 'bmw' from custom plugin was added to globals.
+```
+
+Now Herbie can use your custom template
+
+```python
+H = Herbie("2025-01-01", model="hrrr_analysis")
+```
+
+```python
+H = Herbie("2025-01-01", model="bmw", domain='3a')
+
+# This will never find anything because it is a fictitious model.
+```
--- src/herbie_plugin_tutorial/__init__.py
+++ src/herbie_plugin_tutorial/__init__.py
@@ -1,2 +1,106 @@
-def hello() -> str:
-    return "Hello from herbie-plugin-tutorial!"
+"""Custom model templates for Herbie.
+
+DESCRIPTION : str
+    A description of the model. Give the full name and the
+    domain, if relevant. Just infor for the user.
+DETAILS : dict
+    Some additional details about the model. Provide links
+    to web documentation. Just info for the user.
+PRODUCTS : dict
+    Models usually have different product types. The keys are
+    used in building the GRIB2 source URL.
+    ORDER MATTERS -- If product is None, then Herbie uses the first
+    as default.
+    *ONLY ONE IS USED (FIRST IS USED IF NOT SET)*
+SOURCES : dict
+    Build the URL for the GRIB2 file for different sources.
+    The parameters are from arguments passed into the
+    ``herbie.core.Herbie()`` class.
+    ORDER MATTERS -- If priority is None, then Herbie searches the
+    sources in the order given here.
+    *LOOP THROUGH ALL SOURCES*
+LOCALFILE : str
+    The local file to save the model output. The file will be saved in
+    ``save_dir/model/YYYYmmdd/localFile.grib2``
+    It is sometimes necessary to add details to maintain unique
+    filenames (e.g., rrfs needs to have the member number in LOCALFILE).
+EXPECT_IDX_FILE : {None, "remote"}
+    If None, Herbie knows not to expect an index file for this kind
+    of file. (Perhaps the file is not a GRIB file.)
+    If "remote", then Herbie will expect an index file on the remote
+    location. This is the default value.
+
+
+Optional
+--------
+IDX_SUFFIX : list
+    Default value is ["grib.idx"], which is pretty standard.
+    But for some, like RAP, the idx files are messy and could be a few
+    different styles.
+    self.IDX_SUFFIX = [".grb2.inv", ".inv", ".grb.inv"]
+    *LOOP THROUGH ALL SUFFIXES TO FIND AN INDEX FILE*
+
+IDX_STYLE : {'wgrib2', 'eccodes'}
+    This defines how the index will be interpreted.
+    - NCEP products use ``wgrib2`` to create index files.
+    - ECMWF products use ``eccodes`` to create index files.
+
+Building the file path or URL
+-----------------------------
+Extra arguments passed to the Herbie class are used to build the file
+path or URL. The arguments are:
+- model: str
+    The model name. This is used to build the file path or URL.
+- date: datetime
+    The date of the model run. This is used to build the file path or URL.
+- fxx: int
+    The forecast hour. This is used to build the file path or URL.
+- product: str
+    The product type. This is used to build the file path or URL.
+
+```python
+Herbie(date='2025-01-01', model='hrrr_analysis', fxx=0, product='prs')
+```
+
+Any extra arguments passed to the Herbie class may also used to build
+the file (e.g, member, run, domain, or anything else you want).
+
+```python
+Herbie(date='2025-01-01', model='bmw', fxx=0, member=1, domain='3a')
+```
+"""
+
+
+class hrrr_analysis:
+    """Custom template for HRRR analysis data on AWS S3."""
+
+    def template(self):
+        self.DESCRIPTION = "High-Resolution Rapid Refresh - CONUS: ANALYSIS ONLY"
+        self.DETAILS = {
+            "custom model template": "https://github.com/blaylockbk/herbie-plugin-tutorial",
+        }
+        self.PRODUCTS = {
+            "sfc": "2D surface level fields; 3-km resolution",
+            "prs": "3D pressure level fields; 3-km resolution",
+            "nat": "Native level fields; 3-km resolution",
+            "subh": "Subhourly grids; 3-km resolution",
+        }
+        self.SOURCES = {
+            "aws": f"https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.{self.date:%Y%m%d}/conus/hrrr.t{self.date:%H}z.wrf{self.product}f00.grib2",
+        }
+
+
+class bwm:
+    """Custom template for Brian's Model of Weather (BMW) data."""
+
+    def template(self):
+        self.DESCRIPTION = "Brian's Model of Weather"
+        self.DETAILS = {
+            "local": "These GRIB2 files are produced by a local of the BMW model."
+        }
+        self.PRODUCTS = {
+            "default": "default model product",
+        }
+        self.SOURCES = {
+            "local_main": f"/path/to/bmw/model/output/{self.model}/gribfiles/{self.date:%Y%m%d%H}/my_file.t{self.date:%H}z.f{self.fxx:02d}_{self.domain}.grib2",
+        }
